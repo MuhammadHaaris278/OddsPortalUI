748e47d6a2bf8055485ad99423412ebef8203e43
Muhammad Haaris <muhammad.haaris2003@gmail.com>
2025-07-12 22:46:23 +0500

    Delete postinstall.sh

--- postinstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Set the Playwright browser installation path to a writable directory
-PLAYWRIGHT_BROWSERS_PATH=/opt/render/.cache/ms-playwright python3 -m playwright install --with-deps
